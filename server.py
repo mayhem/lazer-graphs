598c31161f9f930fb30b48cc3c8e3cee74912504
Robert Kaye <rob@musicbrainz.org>
2016-01-01 23:05:41 +0100

    Basic polling/web page display working

--- server.py
+++ server.py
@@ -1,33 +1,95 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
-from flask.ext.socketio import SocketIO, emit
+import os
+import threading
+import atexit
+import copy
+from random import randrange
+from flask import Flask, render_template, request, jsonify
 
 HOST = "0.0.0.0"
 PORT = 8000
+POLL_TIME = 1
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'lazers are kewl'
-app.config['TEMPLATE_DIR'] = "template"
-app.config['STATIC_DIR'] = "static"
-app.config['STATIC_URL'] = "/static"
-socketio = SocketIO(app)
+current_data = {}
+lock = threading.Lock()
+poll_thread = None
+
+def read_data():
+    return read_data_faux()
+
+TEMP_RANGE = 5.0
+POWER_RANGE = 2.0
+CURRENT_RANGE = 4.0
+
+def read_data_faux():
+
+    data = copy.copy(current_data)
+    if not 'temp' in data:
+        data['temp'] = 20.0
+    if not 'power' in data:
+        data['power'] = 5.0 
+    if not 'current' in data:
+        data['current'] = 2.0
+
+    data['temp'] += max(0, randrange(-TEMP_RANGE, TEMP_RANGE))
+    data['current'] += max(0, randrange(-CURRENT_RANGE, CURRENT_RANGE))
+    data['power'] += max(0, randrange(-POWER_RANGE, POWER_RANGE))
+
+    return data
+
+
+def create_app():
+    app = Flask(__name__,
+                static_url_path = "/static",
+                static_folder = "static",
+                template_folder = "template")
+    app.config['SECRET_KEY'] = 'lazers are kewl'
+
+    def interrupt():
+        global poll_thread
+        if poll_thread:
+            poll_thread.cancel()
+
+    def poll_data():
+        global poll_thread
+        global current_data
+
+        lock.acquire()
+        current_data = read_data()
+        lock.release()
+
+        poll_thread = threading.Timer(POLL_TIME, poll_data, ())
+        poll_thread.start()
+
+    def poll_start():
+        global poll_thread
+
+        poll_thread = threading.Timer(POLL_TIME, poll_data, ())
+        poll_thread.start()
+
+    poll_start()
+    atexit.register(interrupt)
+    return app
+
+app = create_app()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('command', namespace='/control')
-def control(message):
-    emit('command', {'data': message['data']})
-
-@socketio.on('connect', namespace='/control')
-def test_connect():
-    emit('status', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/control')
-def test_disconnect():
-    print('Client disconnected')
+@app.route('/api/data')
+def data():
+    lock.acquire()
+    data = copy.copy(current_data)
+    lock.release()
+    return jsonify(data)
 
 if __name__ == '__main__':
-    socketio.run(app, host=HOST, port=PORT, debug=True)
+    def cancel_thread():
+        if poll_thread:
+            poll_thread.cancel()
+
+    atexit.register(cancel_thread)
+
+    app.run(host=HOST, port=PORT, debug=False)
